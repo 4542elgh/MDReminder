20bf303cd5d8297446926d612414ec32df7e4054
Hue Ngo <hue.ngo1992@gmail.com>
2018-12-12 22:17:51 -0800

    add EncodeDecodeUrl class

--- app/src/main/java/com/example/herr/MDReader/NetworkUtils.java
+++ app/src/main/java/com/example/herr/MDReader/NetworkUtils.java
@@ -76,36 +76,4 @@ public class NetworkUtils {
         }
     }
 
-    // encode url Strings for QR Reader
-    // PARAM_FIELD e.g. indications_and_usage
-    // PARAM_LOOKUP e.g. "cough"
-//    public static String encodeUrl() {
-//
-//        String ndcUrl = "";
-//
-//        try {
-//            ndcUrl = OPENFDA_BASE_URL + URLEncoder.encode(PARAM_FIELD + ":" + PARAM_LOOKUP + "&" +PARAM_LIMIT + limitValue, "UTF-8");
-//
-//        } catch (UnsupportedEncodingException e) {
-//            e.printStackTrace();
-//        }
-//
-//        return ndcUrl;
-//    }
-//
-//    // evan can use the code below to decode the encodedUrl
-//    // decode url Strings once scanned in QR Reader
-//    public static String decodeUrl(String encodedUrl) {
-//        String decoded_url = "";
-//
-//        try {
-//            decoded_url = URLDecoder.decode(encodedUrl, "UTF-8");
-//
-//        } catch (UnsupportedEncodingException e) {
-//            e.printStackTrace();
-//        }
-//        return decoded_url;
-//    }
-
-
 }
